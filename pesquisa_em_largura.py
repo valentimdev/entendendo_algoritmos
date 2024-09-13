fbbfb363adcb072ad6ccbe9774b1d753739649bd
valentimdev <valentimdesenvolvedor@gmail.com>
2024-09-13 16:18:24 -0300

    feat:novo algoritmo adicionado

---
+++ pesquisa_em_largura.py
@@ -0,0 +1,29 @@
+from collections import deque
+grafo = {}
+grafo["voce"] = ["alice","bob","claire"]
+grafo["bob"] = ["anuj","peggy"]
+grafo["alice"] = ["peggy"]
+grafo["claire"] = ["thom","jonny"]
+grafo["anuj"]=[]
+grafo["peggy"]=[]
+grafo["thom"]=[]
+grafo["jonny"]=[]
+def pesquisa(nome):
+    fila_de_pesquisa = deque()
+    fila_de_pesquisa+=grafo[nome]
+    verificadas=[]
+    while fila_de_pesquisa:
+        pessoa=fila_de_pesquisa.popleft()
+        if not pessoa in verificadas:
+            if pessoa_e_vendedor(pessoa):
+                print (pessoa +" é um vendedor de manga!")
+                return True
+            else:
+                fila_de_pesquisa+=grafo[pessoa]
+                verificadas.append(pessoa)
+    return False
+
+def pessoa_e_vendedor(nome):
+    return nome[-1] =='m'
+
+pesquisa("voce")
